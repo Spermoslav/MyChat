d43dcfdeddaf046f12b72b89af0591b8e6e4170f
Leonid <114904902+Spermoslav@users.noreply.github.com>
2024-10-22 20:54:34 +0300

    add styles for Widget

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     Widget w;
     w.resize(350, 650);
+    w.setStyleSheet("background-color: rgb(0, 191, 255);"
+                    "border: 1px solid black;"
+                    "border-radius: 4px;");
     w.show();
     return a.exec();
 }
